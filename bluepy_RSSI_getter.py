42a65cb74dc7dc280bd3f260335706a87fd3120d
Aled Jackson <axj714@cs.bham.ac.uk>
2019-10-26 14:34:13 +0100

    hopefully fixed bug

--- bluepy_RSSI_getter.py
+++ bluepy_RSSI_getter.py
@@ -8,31 +8,32 @@ print "scanning for device with manufacture data |" + manufacturer + "|"
 # import the necessary parts of the bluepy library
 from bluepy.btle import Scanner, DefaultDelegate
 
+
+global_RSSIs = []
 # create a delegate class to receive the BLE broadcast packets
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
-        self._RSSIs = []
 
 
     # when this python script discovers a BLE broadcast packet, print a message with the device's MAC address
     def handleDiscovery(self, dev, isNewDev, isNewData):
         for (adtype, desc, value) in dev.getScanData():
                 if desc == "Manufacturer" and value == manufacturer:
-                    self._RSSIs.append(dev.rssi)
+                    global_RSSIs.append(dev.rssi)
 
     def gatherAverageRSSI(self,manufacturer, n_samples):
-        self._RSSIs = []
-        while len(self._RSSIcount) < n_samples:
+        global_RSSIs = []
+        while len(global_RSSIs) < n_samples:
             scanner.scan(1.0)
-        mean_rssi = mean(self._RSSIs)
-        std_dev = std(self._RSSIs)
+        mean_rssi = mean(global_RSSIs)
+        std_dev = std(global_RSSIs)
 
         cutoff_rssi = []
 
         lower_cutoff = mean_rssi - std_dev
         upper_cutoff = mean_rssi + std_dev
-        for x in self._RSSIs:
+        for x in global_RSSIs:
             if not (x < lower_cutoff or x > upper_cutoff):
                 cutoff_rssi.append(x)
         
