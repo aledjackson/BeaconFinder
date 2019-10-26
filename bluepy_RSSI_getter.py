974e903544e0f72b895a425536c64424d8f4e057
Aled Jackson <axj714@cs.bham.ac.uk>
2019-10-26 14:24:41 +0100

    improved RSI getter to get an average and cutoff outliers

--- bluepy_RSSI_getter.py
+++ bluepy_RSSI_getter.py
@@ -12,20 +12,40 @@ from bluepy.btle import Scanner, DefaultDelegate
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
+        self._RSSIs = []
+
 
     # when this python script discovers a BLE broadcast packet, print a message with the device's MAC address
     def handleDiscovery(self, dev, isNewDev, isNewData):
         for (adtype, desc, value) in dev.getScanData():
                 if desc == "Manufacturer" and value == manufacturer:
-                    print "RSSI : " + str(dev.rssi)
+                    self._RSSIs.append(dev.rssi)
 
+    def gatherAverageRSSI(self,manufacturer, n_samples):
+        self._RSSIs = []
+        while len(self._RSSIcount) < n_samples:
+            scanner.scan(1.0)
+        mean_rssi = mean(self._RSSIs)
+        std_dev = std(self._RSSIs)
+
+        cutoff_rssi = []
+
+        lower_cutoff = mean_rssi - std_dev
+        upper_cutoff = mean_rssi + std_dev
+        for x in self._RSSIs:
+            if not (x < lower_cutoff or x > upper_cutoff):
+                cutoff_rssi.append(x)
+        
+        return mean(cutoff_rssi)
+
+
+        
+        return float(self._RSSIsum) / float(self._RSSIcount)
 
 
 
 # create a scanner object that sends BLE broadcast packets to the ScanDelegate
 scanner = Scanner().withDelegate(ScanDelegate())
 
-while True:
-    scanner.scan(10.0)
-
+scanner.gatherAverageRSSI(manufacturer, 10)
 
