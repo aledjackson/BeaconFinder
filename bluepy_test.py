1f98de89edea75b2669a3b554527fcda0d456c5a
Aled Jackson <axj714@cs.bham.ac.uk>
2019-10-26 17:39:01 +0100

    a

--- bluepy_test.py
+++ bluepy_test.py
@@ -24,7 +24,8 @@ for dev in devices:
     print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
 
     for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+        if (desc == "Complete Local Name"):
+            print "  %s = %s" % (desc, value)
 
 
 
